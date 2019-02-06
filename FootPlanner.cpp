926af93fb3912c36bbaf44b3ece44566bf773e86
Akira794 <s1426079kv@s.chibakoudai.jp>
2019-02-06 17:14:14 +0900

    modified method name

---
+++ FootPlanner.cpp
@@ -0,0 +1,142 @@
+#include "FootPlanner.h"
+
+bool FootPlanner::SetTargetPos(const double target_x, const double target_y, const double target_th, FootStatus ref_leg_sup, WalkingStatus ref_walking_status)
+{
+	target_pos.x	 = target_x;
+	target_pos.y	 = target_y;
+	target_pos.th	 = target_th;
+	next_leg_support = ref_leg_sup;
+	walking_status	 = ref_walking_status;
+    int th_foot_step_count = 0;
+
+	foot_step_list_clear();
+
+	target_distance = sqrt(pow(target_x, 2) + pow(target_y, 2));
+
+    if(target_pos.x == 0 && target_pos.y == 0 && target_pos.th == 0.0){
+        stride.x = stride.y = stride.th = 0.0;
+
+    }else if(fabs(target_pos.x) > fabs(target_pos.y)){
+        foot_step_count = static_cast<int>((fabs(target_pos.x) + stride.x - 0.001) / stride.x);
+        if(target_pos.x < 0) stride.x *= -1.0f;
+        stride.y = target_pos.y / foot_step_count;
+    }else{
+        foot_step_count = static_cast<int>((fabs(target_pos.y) + stride.y - 0.001) / stride.y);
+        if(target_pos.y < 0) stride.y *= -1.0f;
+        stride.x = target_pos.x / foot_step_count;
+    }
+
+    if(target_pos.th != 0.0){
+        th_foot_step_count = static_cast<int>((fabs(target_pos.th) + stride.th - 0.001) / stride.th);
+        if(target_pos.th < 0.0) stride.th *= -1.0f;
+
+        if(th_foot_step_count == foot_step_count){
+
+        }else if(th_foot_step_count < foot_step_count){
+            stride.th = target_pos.th / foot_step_count;
+
+        }else{
+            foot_step_count = th_foot_step_count;
+        }
+
+    }else{
+        stride.th = 0.0;
+    }
+/*
+    cout << endl << "th_foot_count = " << th_foot_step_count << endl;
+    cout << endl;
+    cout << "total_stride(x,y,th) = "<< stride.x << ","<< stride.y << "," << stride.th * 180/3.14 << "(deg) "<< endl;
+    cout << "total_foot_step_count=" << foot_step_count << endl;
+*/
+	bool result = target_pos_2_foot_step(ref_leg_sup);
+	return result;
+}
+
+bool FootPlanner::target_pos_2_foot_step(FootStatus ref_leg_sup)
+{
+    start_walk(ref_leg_sup);
+
+    for(int i = 1; i < foot_step_count; i++){
+
+        shift_y = foot_y * ((i%2)*2-1)*2*(-1);
+        next_control_point.x = control_point.x + stride.x*cos(control_point.th) - (stride.y + shift_y)*sin(control_point.th);
+        next_control_point.y = control_point.y + stride.x*sin(control_point.th) + (stride.y + shift_y)*cos(control_point.th);
+        next_control_point.th= control_point.th + stride.th;
+
+        time += step_time;
+        foot_step_list.push_back(Vector4d(time, next_control_point.x, next_control_point.y, next_control_point.th));
+        control_point = next_control_point;
+    }
+
+    stop_walk();
+
+	return true;
+}
+
+double FootPlanner::DistanceTargetPos(Pos2D current_pos)
+{
+    return static_cast<double>(sqrt(pow((target_pos.x-current_pos.x),2)+pow((target_pos.y),2)));
+}
+
+void FootPlanner::start_walk(FootStatus ref_leg_sup)
+{
+    if(ref_leg_sup == -1){ //shiftの調整 左右スタート支持脚で注意
+        foot_y = zmp_correct_y;
+    }else if(ref_leg_sup == 1){
+        foot_y = -zmp_correct_y;
+    }
+
+    control_point = {0.0f, 0.0f, 0.0f};
+    next_control_point = {0.0f, 0.0f, 0.0f};
+
+    if(walking_status == Start){
+        foot_step_list.push_back(Vector4d(time, control_point.x, control_point.x, control_point.th));
+        time += step_time;
+    }
+
+    next_control_point.x  = control_point.x ;
+    next_control_point.y  = control_point.y + foot_y;
+    next_control_point.th = control_point.th;
+    foot_step_list.push_back(Vector4d(time, next_control_point.x, next_control_point.y, control_point.th));
+
+    control_point = next_control_point;
+}
+
+void FootPlanner::stop_walk()
+{
+    next_control_point = target_pos;
+
+    if(shift_y == -0.1){
+        next_control_point.x = control_point.x + stride.x*cos(next_control_point.th) - (stride.y - shift_y)*sin(next_control_point.th);
+        next_control_point.y = control_point.y + stride.x*sin(next_control_point.th) + (stride.y - shift_y)*cos(next_control_point.th);
+        if(walking_status != Stop){
+            time += step_time;
+            foot_step_list.push_back(Vector4d(time, next_control_point.x, next_control_point.y, next_control_point.th));
+        }
+    }else if(shift_y == 0.1){
+        next_control_point.x = control_point.x + stride.x*cos(next_control_point.th) - (stride.y - shift_y)*sin(next_control_point.th);
+        next_control_point.y = control_point.y + stride.x*sin(next_control_point.th) + (stride.y - shift_y)*cos(next_control_point.th);
+
+        if(walking_status != Stop){
+            time += step_time;
+            foot_step_list.push_back(Vector4d(time, next_control_point.x, next_control_point.y, next_control_point.th));
+        }
+    }
+
+    if(shift_y == -0.1){
+        next_control_point.x += zmp_correct_y*cos(next_control_point.th - (M_PI/2));
+        next_control_point.y += zmp_correct_y*sin(next_control_point.th - (M_PI/2));
+    }else if(shift_y == 0.1){
+        next_control_point.x += zmp_correct_y*cos(next_control_point.th + (M_PI/2));
+        next_control_point.y += zmp_correct_y*sin(next_control_point.th + (M_PI/2));
+    }
+
+    time += step_time;
+
+    foot_step_list.push_back(Vector4d(time, next_control_point.x, next_control_point.y, next_control_point.th));
+
+    next_leg_support = BothLeg;
+
+    foot_step_list.push_back(Vector4d(time+2.0f, next_control_point.x, next_control_point.y, next_control_point.th));
+
+}
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "FootPlanner.h"
 #include "CapturePointControl.h"
 #include <string>
 #include <sstream>
@@ -5,11 +6,79 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+// Change for your robot and walking cycle
+static const double FOOT_WIDTH			= 0.050;
+static const double HEIGHT_ZC			= 0.27;
+static const double ZMP_RANGE			= 0.060;
+static const double WALKING_HALF_CYCLE	= 0.340;
+static const double SAMPLING_TIME		= 0.010;
+static const double MAX_X_STEP			= 0.060;
+static const double MAX_Y_STEP			= 0.060;
+static const double MAX_W_STEP          = 0.200;
+
+double rad2deg(double radian){
+    return radian * 180/M_PI;
+}
+
+double deg2rad(int deg){
+    return deg * M_PI / 180;
+}
+
 int main(int argc, char *argv[])
 {
-    CapturePointControl capture_point_control(0.32, 0.01);
+#if 0
+    if (argc != 10 && argc != 11) {
+		std::cout << "Generate a walking pattern." << std::endl <<
+			"Usage: GenerateWalkPattern <target_x> <target_y> <target_th> <initial_speed_x> <initial_speed_y> " <<
+			"<initial_cog_x> <initial_cog_y> <support leg 0:right 1:left> <0:start 1:walking 2:stop> [<filename>]" << std::endl <<
+			"[Caution] If you do not input the <filename>, " << std::endl <<
+			"1) a walking pattern for a half of the walking cycle is calculated, and" << std::endl <<
+			"2) the cofficients for the Pre-Calculated Preview Control is displayed." << std::endl;
+		return -1;
+	}
+	double target_x = atof(argv[1]);
+	double target_y = atof(argv[2]);
+    double target_th = deg2rad(atof(argv[3]));
+	double speed_x = atof(argv[4]);
+	double speed_y = atof(argv[5]);
+	double cog_x = atof(argv[6]);
+	double cog_y = atof(argv[7]);
+	FootStatus foot_status = (atoi(argv[8]) == 1) ? RightLeg : LeftLeg;
+	WalkingStatus walking_status = (atoi(argv[9]) == 1) ? Walking : Start;
+	if (atoi(argv[9]) >= 2) walking_status = Stop;
+	int stop_status = atoi(argv[9]) - 2;	//0:first half,1:second half
+	//std::cout << "stop status:" << stop_status << std::endl;
+	bool save_file = (argc == 11) ? true : false;
+	std::ofstream writing_file;
+	if (save_file) {
+		writing_file.open(argv[10]);
+		writing_file << "time(s) ref_x(m) zmp_x(m) cog_x(m) ref_y(m) zmp_y(m) cog_y(m) vel_x(m) vel_y(m) acc_x(m) acc_y(m)" << std::endl;
+	}
+	Vector2d com_pos, com_vel, com_acc;
+	com_pos(0) = cog_x;
+	com_pos(1) = cog_y;
+	com_vel(0) = speed_x;
+	com_vel(1) = speed_y;
+	int sampling_num_half_cycle = (WALKING_HALF_CYCLE + SAMPLING_TIME / 2) / SAMPLING_TIME;
+
+    FootPlanner plan_node(SAMPLING_TIME, WALKING_HALF_CYCLE);
+    plan_node.SetFootStepParameter(MAX_X_STEP, MAX_Y_STEP, MAX_W_STEP, FOOT_WIDTH, WALKING_HALF_CYCLE);
+    plan_node.SetTargetPos(target_x, target_y, target_th, foot_status, walking_status);
+
+    #if 0
+    	std::cout << "Results of foot steps" << std::endl;
+    	for (int i = 0; i < plan_node.foot_step_list.size(); i ++)
+    		std::cout << plan_node.foot_step_list[i][0] << ","
+            << plan_node.foot_step_list[i][1] << ", "
+            << plan_node.foot_step_list[i][2] << ", "
+            << plan_node.foot_step_list[i][3] * 180/3.14 << "(deg) "<< ", "
+
+            << std::endl;
+    #endif
+#endif
+    CapturePointControl capture_point_control(WALKING_HALF_CYCLE, SAMPLING_TIME);
     capture_point_control.set_footstep();
-    capture_point_control.ref_cp_petterngenerator();
+    capture_point_control.ref_cp_patterngenerator();
     capture_point_control.calc_cog_trajectory();
     capture_point_control.plot_gait_pattern_list();
     capture_point_control.plot_gait_pattern_list_x_time();
